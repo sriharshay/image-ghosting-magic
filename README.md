cdd15d5b74ad7e11a3f2969a6e78b64a64629f59
Sriharsha <Sriharsha.Yaratapalli@alliancedata.com>
2023-05-29 01:00:35 +0530

    Initial commit & Add image filter effects via simple button elements

---
+++ README.md
@@ -0,0 +1,7 @@
+# Beginner HTML (HyperText Markup Language) site scripted
+
+Simple page with image to demonstrate image fitler rules like blur, hue and invert
+
+## Live site
+
+[Run the example live](https://image-ghosting-magic.github.io//).
---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+
+const disappearBtn = document.querySelector('button.image-disappear');
+const appearBtn = document.querySelector('button.image-appear');
+const changeColorBtn = document.querySelector('button.image-change-color');
+const xRayBtn = document.querySelector('button.image-invert');
+
+let dollImg = document.querySelector('img');
+const blurCounter = 5;
+let counter = 0;
+let invertCounter = 0;
+let hueCounter = 0;
+
+// Image blur effect
+function blurImage() {
+  counter++;
+  dollImg.setAttribute('style', 'filter: blur(' + blurCounter * counter + 'px);')
+}
+
+// Image unblur effect
+function unblurImage() {
+  if (counter <= 0) {
+    dollImg.setAttribute('style', 'filter: blur(0px);')
+  } else {
+    counter--;
+    dollImg.setAttribute('style', 'filter: blur(' + blurCounter * counter + 'px);')
+  }
+}
+
+// Image hue effect
+function changeColor() {
+  if (hueCounter >= 360) {
+    hueCounter = 0;
+  } else {
+    hueCounter = hueCounter + 45;
+  }
+  dollImg.setAttribute('style', 'filter: hue-rotate(' + (hueCounter) + 'deg);')
+
+}
+
+// Image invert effect
+function xRayImage() {
+  if (invertCounter) {
+    invertCounter = 0;
+    dollImg.setAttribute('style', 'filter: invert(' + invertCounter + ');')
+  } else {
+    invertCounter = 1;
+    dollImg.setAttribute('style', 'filter: invert(' + invertCounter + ');')
+  }
+}
+
+//Event listeners
+disappearBtn.onclick = function () {
+  blurImage();
+}
+appearBtn.onclick = function () {
+  unblurImage();
+}
+changeColorBtn.onclick = function () {
+  changeColor();
+}
+xRayBtn.onclick = function () {
+  xRayImage();
+}
